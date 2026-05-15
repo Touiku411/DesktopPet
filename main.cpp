80d2e27cd611a5d65a64f10d937cc6a54ce033ef
touiku <touiku@Debian13.lan>
2026-05-16 05:40:11 +0800

    add falling and walking

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <QDebug>
 #include <QMenu>
 #include <QContextMenuEvent>
+#include <QTimer>
+#include <QScreen>
 
 class PetWindow : public QWidget {
     Q_OBJECT
@@ -27,10 +29,59 @@ public:
         layout->addWidget(petLabel);
         // setFixedSize(movie->frameRect().size());
         setFixedSize(150,200);
+
+
+        timer = new QTimer(this);
+        connect(timer, &QTimer::timeout, this, [this](){
+            int nextX = this->x();
+            int nextY = this->y();
+           
+            QRect screenRect = QGuiApplication::primaryScreen()->availableGeometry();
+            int GroundY = screenRect.y() + screenRect.height() - this->height();
+            bool isDrag = QGuiApplication::mouseButtons() & Qt::LeftButton;
+            bool inAir = (this->y() < GroundY);
+            if(isDrag){
+                // 滑鼠拖曳中，暫停重力影響
+                isFalling = false;
+                speedY = 0;
+            }
+            else if(inAir){
+                // 在空中
+                isFalling = true;
+                speedY += 1; // 重力加速度
+                nextY += speedY;
+                if(nextY >= GroundY){
+                    nextY = GroundY;
+                    speedY = 0;
+                    isFalling = false;
+                }
+            }
+            else{
+                // 在地面上
+                isFalling = false;
+                speedY = 0;
+            }
+            if(isWalking && !isFalling){
+                nextX += speedX;
+                if(nextX <= screenRect.x()){
+                    nextX = screenRect.x();
+                    speedX = abs(speedX);
+                }
+                else if(nextX + this->width() >= screenRect.width()){
+                    nextX = screenRect.width() - this->width();
+                    speedX = -abs(speedX);
+                }
+            }
+            if(this->x() != nextX || this->y() != nextY){
+                this->move(nextX, nextY);
+            }
+        });
+        timer->start(16); // ~60 FPS
+
     }
 protected:
     void mousePressEvent(QMouseEvent *event) override{
-        if(event->button() == Qt::LeftButton){
+        if( !isWalking && !isFalling && event->button() == Qt::LeftButton){
             if(windowHandle()){
                 windowHandle()->startSystemMove();
             }
@@ -43,7 +94,19 @@ protected:
     }
     void mouseDoubleClickEvent(QMouseEvent *event)override{
         if(event->button() == Qt::LeftButton){
-            qDebug() << "好痛痛!";
+            if(isFalling){
+                event->accept();
+                return;
+            }
+            isWalking = !isWalking;
+            if(!isWalking){
+                qDebug() << "Stop Walking:" << isWalking;
+            }
+            else{
+                qDebug() << "Walking:" << isWalking;
+            }
+            
+            
             event->accept();
         }
     }
@@ -56,6 +119,11 @@ protected:
     
 private:
     QMovie *movie;
+    QTimer *timer;
+    int speedX = 4;
+    int speedY = 12;
+    bool isWalking = false;
+    bool isFalling = false;    
 } ;
 int main(int argc, char *argv[]){
 
